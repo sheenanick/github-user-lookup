1592ac57c364ef30a287d657672fd9c78d5cad20
Sheena Nickerson <sheenanick@gmail.com>
2016-10-14 08:24:59 -0700

    add validation for if no user is found

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,8 +1,12 @@
 var User = require('./../js/user.js').userModule;
 
 var displayRepos = function(repoNames, repoDescriptions) {
-  for (var i = 0; i < repoNames.length; i++) {
-    $('#repos').append('<li>' + repoNames[i] + ', <em>' + repoDescriptions[i] + '</em></li>');
+  if(repoNames[0]) {
+    for (var i = 0; i < repoNames.length; i++) {
+      $('#repos').append('<li>' + repoNames[i] + ', <em>' + repoDescriptions[i] + '</em></li>');
+    }
+  } else {
+    alert('user not found');
   }
 }
 
--- js/user.js
+++ js/user.js
@@ -8,14 +8,17 @@ User.prototype.getRepos = function(username, displayFunction) {
     console.log(response);
     var repoNames = [];
     var repoDescriptions = [];
-    for (var i = 0; i < response.length; i++) {
-      repoNames.push(response[i].name);
-      if (response[i].description) {
-        repoDescriptions.push(response[i].description);
-      } else {
-        repoDescriptions.push('no description');
+    if (response.length > 0) {
+      for (var i = 0; i < response.length; i++) {
+        repoNames.push(response[i].name);
+        if (response[i].description) {
+          repoDescriptions.push(response[i].description);
+        } else {
+          repoDescriptions.push('no description');
+        }
       }
-
+    } else {
+      repoNames.push(false);
     }
     displayFunction(repoNames, repoDescriptions);
   }).fail(function(error){

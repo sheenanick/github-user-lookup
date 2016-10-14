2d362fd6cd09d9b5011286796a96c004f3be4ad1
Sheena Nickerson <sheenanick@gmail.com>
2016-10-14 10:42:43 -0700

    add getProfile method and separate it from getRepos method

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,13 +1,17 @@
 var User = require('./../js/user.js').userModule;
 
+var displayProfile = function(photo, followers, userDate, name, email) {
+  $('#profile').text(followers + photo + userDate + name + email);
+};
+
 var displayRepos = function(repoNames, repoDescriptions, htmlUrl, dateCreated) {
   if(repoNames[0]) {
     for (var i = 0; i < repoNames.length; i++) {
-      $('#repos').append('<div class="col-sm-3 repo-col">' +
+      $('#repos').append('<div class="col-sm-3 repo-col"><div class="col-box">' +
                           '<h4><a href="' + htmlUrl[i] + '" target="_blank">' + repoNames[i] + '</a></h4>' +
                           '<p><em>' + repoDescriptions[i] + '</em></p> ' +
                           '<p>Date Created: ' + dateCreated[i] + '</p>' +
-                        '</div>');
+                        '<div></div>');
     }
   } else {
     $('#repos').append('<div class="col-sm-3 repo-col">' +
@@ -17,15 +21,22 @@ var displayRepos = function(repoNames, repoDescriptions, htmlUrl, dateCreated) {
 };
 
 var displayError = function() {
+  userFound = false;
   alert('user not found');
-}
+};
+
+var username;
+var userFound = true;
 
 $(function() {
   var user = new User();
   $('form').submit(function(event) {
     event.preventDefault();
     $('#repos').html("");
-    var username = $('#username').val();
-    user.getRepos(username, displayRepos, displayError);
+    username = $('#username').val();
+    user.getProfile(username, displayProfile, displayError);
+    if (userFound) {
+      user.getRepos(username, displayRepos);
+    }
   });
 });
--- js/user.js
+++ js/user.js
@@ -3,32 +3,48 @@ var apiKey = require('./../.env').apiKey;
 function User() {
 }
 
-User.prototype.getRepos = function(username, displayFunction, displayError) {
+var repoNames = [];
+var repoDescriptions = [];
+var htmlUrl = [];
+var dateCreated = [];
+var possiblyNull = [];
+
+User.prototype.getProfile = function(username, displayFunction, displayError) {
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
-      console.log(response);
-      var repoNames = [];
-      var repoDescriptions = [];
-      var htmlUrl = [];
-      var dateCreated = [];
-      if (response.length > 0) {
-        for (var i = 0; i < response.length; i++) {
-          repoNames.push(response[i].name);
-          htmlUrl.push(response[i].html_url);        dateCreated.push(moment(response[i].created_at).format('L'));
-          if (response[i].description) {
-            repoDescriptions.push(response[i].description);
-          } else {
-            repoDescriptions.push('no description');
-          }
-        }
-      } else {
-        repoNames.push(false);
+    var photo = response.avatar_url;
+    var followers = response.followers;
+    var userDate = response.created_at;
+    possiblyNull.push(response.name);
+    possiblyNull.push(response.email);
+    for (var i = 0; i < possiblyNull.length; i++) {
+      if (possiblyNull[i] === null) {
+        possiblyNull[i] = 'not available';
       }
-      displayFunction(repoNames, repoDescriptions, htmlUrl, dateCreated);
-    });
+    }
+    displayFunction(photo, followers, userDate, possiblyNull[0], possiblyNull[1]);
   }).fail(function(error){
     displayError();
+  });
+};
+
+User.prototype.getRepos = function(username, displayFunction) {
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    if (response.length > 0) {
+      for (var i = 0; i < response.length; i++) {
+        repoNames.push(response[i].name);
+        htmlUrl.push(response[i].html_url);        dateCreated.push(moment(response[i].created_at).format('L'));
+        if (response[i].description) {
+          repoDescriptions.push(response[i].description);
+        } else {
+          repoDescriptions.push('no description');
+        }
+      }
+    } else {
+      repoNames.push(false);
+    }
+    displayFunction(repoNames, repoDescriptions, htmlUrl, dateCreated);
   });
 };
 

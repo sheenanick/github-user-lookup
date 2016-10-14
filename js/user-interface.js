21a38c19dc9cfddf87c9671e1da654498c89f191
Sheena Nickerson <sheenanick@gmail.com>
2016-10-14 09:04:10 -0700

    put repo results into columns, and clear results at the beginning of search

--- js/user-interface.js
+++ js/user-interface.js
@@ -3,7 +3,11 @@ var User = require('./../js/user.js').userModule;
 var displayRepos = function(repoNames, repoDescriptions, htmlUrl, dateCreated) {
   if(repoNames[0]) {
     for (var i = 0; i < repoNames.length; i++) {
-      $('#repos').append('<li><a href="' + htmlUrl[i] + '" target="_blank">' + repoNames[i] + '</a>, <em>' + repoDescriptions[i] + '</em> ' + dateCreated[i] + '</li>');
+      $('#repos').append('<div class="col-sm-3 repo-col">' +
+                          '<h4><a href="' + htmlUrl[i] + '" target="_blank">' + repoNames[i] + '</a></h4>' +
+                          '<p><em>' + repoDescriptions[i] + '</em></p> ' +
+                          '<p>Date Created: ' + dateCreated[i] + '</p>' +
+                        '</div>');
     }
   } else {
     alert('user not found');
@@ -14,6 +18,7 @@ $(function() {
   var user = new User();
   $('form').submit(function(event) {
     event.preventDefault();
+    $('#repos').html("");
     var username = $('#username').val();
     user.getRepos(username, displayRepos);
   });
